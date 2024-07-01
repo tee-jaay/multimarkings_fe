77d597b31881aecfd5075442f92de737aaed261b
Tee Jaay <tamjid84@yahoo.com>
2024-07-01 11:39:11 +0600

    post card icons

--- src/_components/common/post/PostItem.tsx
+++ src/_components/common/post/PostItem.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
-import { Card, Col, Container, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faComment, faEnvelope, faGlobe, faLocation, faMap, faPhone, faShare } from '@fortawesome/free-solid-svg-icons';
+import { Card, Col, Container, ListGroup, ListGroupItem, Row, Stack } from 'react-bootstrap';
 
 const PostItem: React.FC = () => {
     return (
@@ -10,35 +12,40 @@ const PostItem: React.FC = () => {
                 <ListGroupItem>
                     <Container>
                         <Row className="no-gutters">
-                            <Col xs="1" className=''>
-                                <Image className='' alt="" src={"/64.jpg"} width={48} height={48} />
+                            <Col xs="1">
+                                <Image alt="" src={"/64.jpg"} width={48} height={48} />
                             </Col>
-                            <Col xs="11" className=''>
+                            <Col xs="11">
                                 <div className='mx-2'>
-                                    <Link href={"/"} className='fw-bold' style={{ fontSize: '15px' }} >Lorem ipsum dolor sit amet consectetur adipisicing elit</Link>
-                                    <div style={{ fontSize: '13px' }}>
-                                        <span>Submitted by <Link href={"/"}>JhonD</Link></span> <span><Link href={"/"}>about 5 days ago</Link></span> <span><a href="/#">share</a></span> <span><Link href={"/#respond"}>0 Comments</Link></span> <span><Link href={"/"}>Costa Rica</Link></span>
-                                    </div>
+                                    <Link href={"/"} className='fw-bold text-decoration-none' style={{ fontSize: '15px' }} >Lorem ipsum dolor sit amet consectetur adipisicing elit</Link>
+                                    <Stack direction='horizontal' gap={2} style={{ fontSize: '12px' }}>
+                                        <span>Submitted by <Link href={"/"} className='text-decoration-none'>JhonD</Link></span>
+                                        <span><FontAwesomeIcon icon={faClock} height={24} width={24} color='#888888' />  <Link href={"/"} className='text-decoration-none'>about 5 days ago</Link></span>
+                                        <span><FontAwesomeIcon icon={faShare} height={24} width={24} color='#888888' />  <a href="/#" className='text-decoration-none'>share</a></span>
+                                        <span><FontAwesomeIcon icon={faComment} height={24} width={24} color='#888888' /><Link href={"/#respond"} className='text-decoration-none'>0 Comments</Link></span>
+                                        <span><FontAwesomeIcon icon={faMap} height={24} width={24} color='#888888' /><Link href={"/"} className='text-decoration-none'>Costa Rica</Link></span>
+                                    </Stack>
                                     {/* URL */}
-                                    <div style={{ fontSize: '13px' }}>
-                                        <span>icon</span> <Link href={"https://devapps.uk"}>https://devapps.uk</Link>
+                                    <div style={{ fontSize: '12px' }} className='mt-2'>
+                                        <span><FontAwesomeIcon icon={faGlobe} height={24} width={24} color='#888888' /></span> <Link href={"https://devapps.uk"} className='text-decoration-none'>https://devapps.uk</Link>
                                     </div>
                                     {/* Phone & Email */}
-                                    <div style={{ fontSize: '13px' }}>
-                                        <span>icon +1 0123456789</span> <span>icon admin@example.com</span>
+                                    <div style={{ fontSize: '12px' }} className='mt-2'>
+                                        <span><FontAwesomeIcon icon={faPhone} height={24} width={24} color='#888888' /> +1 0123456789</span>
+                                        <span className='mx-3'><FontAwesomeIcon icon={faEnvelope} height={24} width={24} color='#888888' />admin@example.com</span>
                                     </div>
                                     {/* Location */}
-                                    <div style={{ fontSize: '13px' }}>
-                                        <span>Texas</span>
+                                    <div style={{ fontSize: '12px' }} className='mt-2'>
+                                        <span><FontAwesomeIcon icon={faLocation} height={24} width={24} color='#888888' />Texas</span>
                                     </div>
                                 </div>
                             </Col>
                         </Row>
                     </Container>
                     <Container>
-                        <div className='mt-2 mb-1' style={{ fontSize: '13px' }}>
+                        <div className='mt-2 mb-1' style={{ fontSize: '12px' }}>
                             <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut eum, sequi praesentium assumenda id quod voluptates eos amet! Officiis reprehenderit esse vero accusamus doloribus beatae excepturi, libero a natus!</span>
-                            <span className='mx-1'><Link href={"/"}>... Read More</Link></span>
+                            <span className='mx-1'>... <Link href={"/"} className='text-decoration-none'>Read More</Link></span>
                         </div>
                     </Container>
                 </ListGroupItem>
