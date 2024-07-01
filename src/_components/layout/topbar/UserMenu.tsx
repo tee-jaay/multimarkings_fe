72b3c86a1380a5dc376c8c22378df6035b5a68fd
Tee Jaay <tamjid84@yahoo.com>
2024-07-01 12:22:30 +0600

    user menu style

--- src/_components/layout/topbar/UserMenu.tsx
+++ src/_components/layout/topbar/UserMenu.tsx
@@ -1,24 +1,26 @@
 'use client';
+import appVariables from '@/_lib/constants';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 const UserMenu = () => {
     return (
-        <Dropdown>
+        <Dropdown className='user_menu'>
             <Dropdown.Toggle
                 variant="secondary"
                 id="dropdown-basic"
                 size='sm'
-                style={{ fontSize: '12px' }}
+                style={{ fontSize: '12px', backgroundColor: appVariables.color_menu, border: 0, borderRadius: 0 }}
+                className='text-uppercase'
             >
-                User
+                login / register
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1" className="text-capitalize" style={{ fontSize: '12px' }}>new item</Dropdown.Item>
-                <Dropdown.Item href="#/action-2" className="text-capitalize" style={{ fontSize: '12px' }}>my items</Dropdown.Item>
-                <Dropdown.Item href="#/action-3" className="text-capitalize" style={{ fontSize: '12px' }}>settings</Dropdown.Item>
+            <Dropdown.Menu style={{ backgroundColor: appVariables.color_menu, border: 0, borderRadius: 0 }}>
+                <Dropdown.Item href="#/action-1" className="text-capitalize text-light" style={{ fontSize: '12px' }}>new item</Dropdown.Item>
+                <Dropdown.Item href="#/action-2" className="text-capitalize text-light" style={{ fontSize: '12px' }}>my items</Dropdown.Item>
+                <Dropdown.Item href="#/action-3" className="text-capitalize text-light" style={{ fontSize: '12px' }}>settings</Dropdown.Item>
                 <Dropdown.Divider />
-                <Dropdown.Item eventKey="4" className='text-capitalize' style={{ fontSize: '12px' }}>logout</Dropdown.Item>
+                <Dropdown.Item eventKey="4" className='text-capitalize text-light' style={{ fontSize: '12px' }}>logout</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     );
